d8534124c79c1e0e978dd4f89ebdccd9694f5a2e
nickku1223 <101886409+nickku1223@users.noreply.github.com>
2022-07-22 19:22:03 -0400

    Adding retirement_info.csv and queries script

--- schema.sql
+++ schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE departments (
 );
 
 CREATE TABLE employees (
-	 emp_no INT NOT NULL,
+	   emp_no INT NOT NULL,
      birth_date DATE NOT NULL,
      first_name VARCHAR NOT NULL,
      last_name VARCHAR NOT NULL,
@@ -39,12 +39,11 @@ CREATE TABLE salaries (
 
 
 CREATE TABLE titles (
-    emp_no INT NOT NULL,
-    title VARCHAR NOT NULL,
+  emp_no INT NOT NULL,
+  title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE dept_emp(
