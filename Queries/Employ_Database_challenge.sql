b2de1b125e5cce9cdd347c1af1b833c76824c371
nickku1223 <101886409+nickku1223@users.noreply.github.com>
2022-07-23 19:23:32 -0400

    adding query script

--- Queries/Employ_Database_challenge.sql
+++ Queries/Employ_Database_challenge.sql
@@ -28,3 +28,21 @@ INTO retiring_title
 FROM retiree_info 
 GROUP BY title
 ORDER BY COUNT (title) DESC
+
+-- Creating mentorship eligibility table
+SELECT DISTINCT ON (em.emp_no) em.emp_no,
+	em.first_name,
+	em.last_name,
+	em.birth_date,
+	de.from_date,
+	de.to_date,
+	titles.title
+INTO metorship_info
+FROM employees as em
+INNER JOIN dept_emp as de
+	ON (em.emp_no = de.emp_no)
+INNER JOIN titles
+	ON (em.emp_no = titles.emp_no)
+WHERE (de.to_date = '9999-01-01') 
+	AND (em.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no
