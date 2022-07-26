20c7bfb4f95f6a9d0b978acf3c3ff43f6ac7df66
nickku1223 <101886409+nickku1223@users.noreply.github.com>
2022-07-26 13:43:10 -0400

    Adding Challenge sql script

--- Queries/Employ_Database_challenge.sql
+++ Queries/Employ_Database_challenge.sql
@@ -5,7 +5,7 @@ SELECT em.emp_no,
 	titles.title,
 	titles.from_date,
 	titles.to_date
-INTO retirement_info
+INTO retirement_title
 FROM employees as em
 INNER JOIN titles
 	ON (em.emp_no = titles.emp_no)
@@ -14,18 +14,18 @@ ORDER BY em.emp_no
 
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (emp_no) emp_no,
-ri.first_name,
-ri.last_name,
-ri.title
-INTO retiree_info
-FROM retirement_info as ri
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_title
+FROM retirement_title as rt
 WHERE to_date = '9999-01-01'
 ORDER BY emp_no, to_date DESC;
 
 SELECT COUNT (title),
 title
 INTO retiring_title
-FROM retiree_info 
+FROM unique_title 
 GROUP BY title
 ORDER BY COUNT (title) DESC
 
@@ -37,7 +37,7 @@ SELECT DISTINCT ON (em.emp_no) em.emp_no,
 	de.from_date,
 	de.to_date,
 	titles.title
-INTO metorship_info
+INTO mentorship_eligibility
 FROM employees as em
 INNER JOIN dept_emp as de
 	ON (em.emp_no = de.emp_no)
